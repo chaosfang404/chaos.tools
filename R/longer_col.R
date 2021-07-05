d96f398b8697238dad16574078922d1d9b1afb43
chaosfang404 <83016338+chaosfang404@users.noreply.github.com>
2021-07-05 11:39:52 +0800

    Add files via upload

---
+++ R/longer_col.R
@@ -0,0 +1,32 @@
+longer_col = function(
+				.data,
+				column,
+				sep = "[^[:alnum:]]+"
+){
+	split_data <- as.data.table(.data)%>%
+					.[[column]] %>%
+					tstrsplit(split = sep) %>%
+					setDT()
+	split_data %>%
+		setnames(
+			paste0(
+				"Tmp_splited_", 
+				column,
+				"_No.",
+				1:ncol(.)
+			)
+		)
+
+	data_with_na <- .data %>%
+						as.data.table %>%
+						.[,(column):=NULL] %>%
+						.[,names(split_data):=split_data] %>%
+						melt(
+							measure = patterns(paste0("^Tmp_splited_",column,"_No.")),
+							value.name = column
+						) %>%
+						.[,-"variable"]
+    
+	na_remove_cmd <- glue("!is.na({column})")
+	eval(parse(text = glue("data_with_na[{na_remove_cmd}]")))
+}
---
+++ R/separate_col.R
@@ -0,0 +1,43 @@
+separate_col = function(
+					.data,
+					column,
+					into = "tmp_splited_columns_names",
+					sep = "[^[:alnum:]]+",
+					remove = TRUE
+){
+	split_columns <- as.data.table(.data)%>%
+						.[[column]] %>%
+						tstrsplit(split = sep) %>%
+						setDT()
+    
+	if(identical(into,"tmp_splited_columns_names"))
+	{
+		into2 <- paste(
+					"splited",
+					column,
+					seq(1,ncol(split_columns),1),
+					sep = "_"
+				)        
+	} else
+	{
+		into2 <- into
+	}
+  
+	split_columns %>%
+		setnames(into2) 
+
+	if(remove)
+	{
+		.data %>%
+			as.data.table %>%
+			.[,(column):=NULL] %>%
+			.[,names(split_columns):=split_columns] %>%
+			.[]
+	} else
+	{
+		data %>%
+			as.data.table %>%
+			.[,names(split_columns):=split_columns] %>%
+			.[]
+	}
+}
