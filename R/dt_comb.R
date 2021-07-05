c1ea3e284a7df2ef5d024847d4a98406ac7789d1
chao <chaosfang404>
2021-07-05 20:52:51 +0800

    rewrite the str_comb function

--- R/dt_comb.R
+++ R/dt_comb.R
@@ -4,14 +4,9 @@ require(data.table)
 dt_comb <- function(
 				.data,
 				c2,
-				c3 = NA,
-				c4 = NA,
-				c5 = NA,
-				c6 = NA,
-				c7 = NA,
-				c8 = NA
+				...
 ){
-	str_comb(.data,c2,c3,c4,c5,c6,c7,c8) %>%
+	str_comb(.data,c2,...) %>%
 		tstrsplit(split = "_") %>%
 		setDT() %>%
 		.[]   	
--- R/str_comb.R
+++ R/str_comb.R
@@ -1,80 +1,29 @@
 str_comb <- function(
-				c1,
+				.data,
 				c2,
-				c3 = NA,
-				c4 = NA,
-				c5 = NA,
-				c6 = NA,
-				c7 = NA,
-				c8 = NA,
+				...,
 				sep = "_"
-){	
-##========basic_combine================
-    all_2 <- NULL
-	for (i in c1) 
+){
+	all <- NULL
+	for (i in .data)
 	{
-		all_2 <- c(all_2,paste(i,c2,sep = sep))
+		all <- c(all,paste(i,c2,sep = sep))
 	}
-	all <- all_2
-##========3_item_combine================
-	if (unique(!is.na(c3)))
+
+	list_rest <- list(...)
+	list_rest_len <- length(list_rest)
+    
+    if (list_rest_len >= 1)
 	{
-		all_3 <- NULL
-		for(j in all_2)
+		for (i in 1:list_rest_len)
 		{
-			all_3 <- c(all_3,paste(j,c3,sep = sep))
+			all_tmp <- NULL
+			for (j in all)
+			{
+				all_tmp <- c(all_tmp,paste(j,list_rest[[i]],sep = sep))
+			}
+			all <- all_tmp
 		}
-		all <- all_3
-	}
-##========4_item_combine================
-	if (unique(!is.na(c4)))
-	{
-		all_4 <- NULL
-		for(k in all_3)
-		{
-			all_4 <- c(all_4,paste(k,c4,sep = sep))
-		}
-		all <- all_4
-	}
-##========5_item_combine================
-	if (unique(!is.na(c5)))
-	{
-		all_5 <- NULL
-		for(l in all_4)
-		{
-			all_5 <- c(all_5,paste(l,c5,sep = sep))
-		}
-		all <- all_5
-	}
-##========6_item_combine================
-	if (unique(!is.na(c6)))
-	{
-		all_6 <- NULL
-		for(m in all_5)
-		{
-			all_6 <- c(all_6,paste(m,c6,sep = sep))
-		}
-		all <- all_6
-	}
-##========7_item_combine================
-	if (unique(!is.na(c7)))
-	{
-		all_7 <- NULL
-		for(n in all_6)
-		{
-			all_7 <- c(all_7,paste(n,c7,sep = sep))
-		}
-		all <- all_7
-	}
-##========8_item_combine================
-	if (unique(!is.na(c8)))
-	{
-		all_8 <- NULL
-		for(o in all_7)
-		{
-			all_8 <- c(all_8,paste(o,c8,sep = sep))
-		}
-		all <- all_8
 	}
 	all
 }
