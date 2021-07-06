7a8b8757556837ff9dc418471fe6c76802d38ae1
chao <chaosfang404>
2021-07-06 14:05:32 +0800

    fix a bug

--- R/wcpa.R
+++ R/wcpa.R
@@ -1,9 +1,11 @@
-require(data.table)
-
 wcpa() = function(.data
 ){
-	names(.data) <- c("sample","resolution","normalization","chr1","chr2","interaction")
-	.data[
+	require(data.table)
+
+	dt <- data.table(.data)
+
+	names(dt) <- c("sample","resolution","normalization","chr1","chr2","interaction")
+	dt[
 		,
 		.(chr1,chr2,interaction,sample_total = sum(interaction)/2),
 		.(sample,resolution,normalization)	
