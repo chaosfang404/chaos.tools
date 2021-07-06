ab69d6838ec7ec0a71437792e7f36fc916bb1be0
chao <chaosfang404>
2021-07-06 16:08:08 +0800

    rewrite multiplot

--- R/multiplot.R
+++ R/multiplot.R
@@ -1,4 +1,10 @@
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+multiplot <- function(
+				..., 
+				plotlist=NULL, 
+				file, 
+				cols=1, 
+				layout=NULL
+){
 	library(grid)
  
 	# Make a list from the ... arguments and plotlist
@@ -7,7 +13,8 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
 	numPlots = length(plots)
  
 	# If layout is NULL, then use 'cols' to determine layout
-	if (is.null(layout)) {
+	if (is.null(layout)) 
+	{
 		# Make the panel
 		# ncol: Number of columns of plots
 		# nrow: Number of rows needed, calculated from # of cols
@@ -18,16 +25,18 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
 				)
 	}
  
- if (numPlots==1) {
+	if (numPlots==1) 
+	{
 		print(plots[[1]])
-
-	} else {
+	} else
+	{
 		# Set up the page
 		grid.newpage()
 		pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
  
 		# Make each plot, in the correct location
-		for (i in 1:numPlots) {
+		for (i in 1:numPlots) 
+		{
 			# Get the i,j matrix positions of the regions that contain this subplot
 			matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
  
