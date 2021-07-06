9899ba176344699e1b642d8f399dc103955dd72b
chao <chaosfang404>
2021-07-06 13:32:46 +0800

    fix a bug

--- R/wcpa.R
+++ R/wcpa.R
@@ -1,27 +1,27 @@
-wcpa = function(.data
-){
-	require(data.table)
+require(data.table)
 
+wcpa() = function(.data
+){
 	names(.data) <- c("sample","resolution","normalization","chr1","chr2","interaction")
 	.data[
-        ,
-        .(chr1,chr2,interaction,sample_total = sum(interaction)/2),
-        .(sample,resolution,normalization)    
+		,
+		.(chr1,chr2,interaction,sample_total = sum(interaction)/2),
+		.(sample,resolution,normalization)	
 	][
-        ,
-      	.(chr2,interaction,sample_total,chr1_total = sum(interaction)),
-      	.(sample,resolution,normalization,chr1)
-    ][
-        ,
-        .(chr1,interaction,sample_total,chr1_total,chr2_total = sum(interaction)),
-        .(sample,resolution,normalization,chr2)
-    ][
-        ,
-        .(sample,resolution,normalization,chr1,chr2,interaction,sample_total,chr1_total,chr2_total)
-    ][
-        order(sample,resolution,normalization,chr1,chr2)
-    ][
-        ,
-        WCPA := interaction/(((chr1_total/sample_total)*(chr2_total/(sample_total - chr1_total)) + (chr2_total/sample_total)*(chr1_total/(sample_total - chr2_total))) * sample_total/2)
-    ][]
+		,
+	  	.(chr2,interaction,sample_total,chr1_total = sum(interaction)),
+	  	.(sample,resolution,normalization,chr1)
+	][
+		,
+		.(chr1,interaction,sample_total,chr1_total,chr2_total = sum(interaction)),
+		.(sample,resolution,normalization,chr2)
+	][
+		,
+		.(sample,resolution,normalization,chr1,chr2,interaction,sample_total,chr1_total,chr2_total)
+	][
+		order(sample,resolution,normalization,chr1,chr2)
+	][
+		,
+		WCPA := interaction/(((chr1_total/sample_total)*(chr2_total/(sample_total - chr1_total)) + (chr2_total/sample_total)*(chr1_total/(sample_total - chr2_total))) * sample_total/2)
+	][]
 }
