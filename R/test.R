0cdec56c89a31ba38d7704087f51bf3b22eda3db
chao <chaosfang404>
2021-07-06 15:31:18 +0800

    upload a test function

--- R/test.R
+++ R/test.R
@@ -1,4 +1,4 @@
-test = function(.data)
+fun_test = function(.data)
 {
 	names(.data) <- c("sample","resolution","normalization","chr1","chr2","interaction")
 	.data[,.(chr1,chr2,interaction,sample_total = sum(interaction)/2),.(sample,resolution,normalization)][,.(chr2,interaction,sample_total,chr1_total = sum(interaction)),.(sample,resolution,normalization,chr1)][,.(chr1,interaction,sample_total,chr1_total,chr2_total = sum(interaction)),.(sample,resolution,normalization,chr2)][,.(sample,resolution,normalization,chr1,chr2,interaction,sample_total,chr1_total,chr2_total)][order(sample,resolution,normalization,chr1,chr2)][,WCPA := interaction/(((chr1_total/sample_total)*(chr2_total/(sample_total - chr1_total)) + (chr2_total/sample_total)*(chr1_total/(sample_total - chr2_total))) * sample_total/2)][]
