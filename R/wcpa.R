ff06ec594906d2a09f76700d53c30f9c1ac1be7b
chao <chaosfang404>
2021-07-06 14:07:34 +0800

    bump version to 0.0.6

--- R/wcpa.R
+++ R/wcpa.R
@@ -1,10 +1,11 @@
-wcpa() = function(.data
-){
+wcpa = function(.data)
+{
 	require(data.table)
 
 	dt <- data.table(.data)
 
 	names(dt) <- c("sample","resolution","normalization","chr1","chr2","interaction")
+
 	dt[
 		,
 		.(chr1,chr2,interaction,sample_total = sum(interaction)/2),
