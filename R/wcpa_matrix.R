175a2ab5490c6d0705c96a7f1b33919ef49ddea1
chao <chaosfang404>
2021-07-07 15:07:42 +0800

    add function wcpa_matrix wcpa_plot wcpa_diff

---
+++ R/wcpa_matrix.R
@@ -0,0 +1,30 @@
+wcpa_matrix = function(
+				.data,
+				name,
+				res = "2.5Mb",
+				method = "KR",
+				chr = c(1:22,"X","Y")
+){
+	chr_list <- as.character(chr)
+	
+	wcpa(.data)[
+		  sample == name & 
+		  resolution == res &
+		  normalization == method &
+		  chr1 %in% chr_list & 
+		  chr2 %in% chr_list
+	][
+		, 
+		.(
+			chr1 = factor(chr1,levels = chr_list),
+			chr2 = factor(chr2,levels = chr_list),
+			WCPA
+		)
+	] %>%
+   	wider_dt(
+		 chr1,
+		 name = "chr2",
+		 value = "WCPA"
+	 ) %>%
+	tibble::column_to_rownames("chr1")
+}
