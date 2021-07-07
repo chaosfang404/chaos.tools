4a32292117bd2e626be530b7827ce1b1883271ed
chao <chaosfang404>
2021-07-07 15:24:59 +0800

    fix a bug

--- R/wcpa_plot.R
+++ R/wcpa_plot.R
@@ -5,15 +5,6 @@ wcpa_plot = function(
 				norm = "KR",
 				chr = c(1:22,"X","Y")
 ){
-	min <- 0.8
-	step <- 0.001
-	mid <- 1
-	# In this project, 1 means there is no difference between 2 samples.
-	# since 1 is the special mark, we need to define the color of it specifically
-	max <- 2*mid - min
-	#此时，max - mid = mid - min，即两段长度相同
-	#也可以指定max值，使得两段长度不同
-
 	wcpa_matrix(
 				.data,
 				name,
@@ -33,14 +24,7 @@ wcpa_plot = function(
 				number_color = "black",
 				number_format = "%.1f",
 				angle_col ="0",
-				color = c(
-							colorRampPalette(colors = c("#3c5488","white"))(length(seq(min,mid,step))),
-							colorRampPalette(colors = c("white","#e64b35"))(length(seq(mid,max,step)))
-				),
-				#如果两段颜色长度不同，须要分段设置颜色，长度相同时可以合并为一个，分开写更为通用
-				legened_breaks = seq(min,max,(max-min)/4),
-				#4 means 5 numbers from min to max
-				breaks=seq(min,max,step),
+				color = colorRampPalette(colors = c("#3c5488","white","#e64b35"))(1000),
 				scale = "none",
 				border_color = "white",
 				border = T
