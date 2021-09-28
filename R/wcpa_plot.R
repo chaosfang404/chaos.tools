e8525edaa7f5e734c55f7a771a7786b54583d707
chaosfang404 <fc-ibb105@outlook.com>
2021-09-28 19:28:40 +0800

    rewrite wcpa_compare_plot

--- R/wcpa_plot.R
+++ R/wcpa_plot.R
@@ -4,8 +4,8 @@ wcpa_plot = function(
 				res = "2500000",
 				norm = "KR",
 				chr = c(1:22,"X","Y"),
-				font_size = "10",
-				border_color = "white",
+				axis_size = "10",
+				border_color = "#FFFFFF",
 				scales = "fixed"
 ){
 	dt <- wcpa(.data)[
@@ -40,7 +40,7 @@ wcpa_plot = function(
 				) + 
 				scale_fill_gradient2(
 					low = "#3c5488",
-					mid = "white",
+					mid = "#FFFFFF",
 					high = "#e64b35",
 					midpoint = mean(c(min(dt$WCPA),max(dt$WCPA)))
 				) + 
@@ -55,8 +55,9 @@ wcpa_plot = function(
 					panel.grid.minor = element_blank(),
 					axis.ticks.x = element_blank(), 
 					axis.ticks.y = element_blank(),
-					axis.text.x = element_text(size = font_size),
-					axis.text.y = element_text(size = font_size)
+					axis.text.x = element_text(size = axis_size),
+					axis.text.y = element_text(size = axis_size),
+					legend.key.widt = unit(0.4,"cm")
 				)
 
 	if(length(unique(dt$sample)) == 1)
@@ -74,3 +75,72 @@ wcpa_plot = function(
 	}
 
 }
+
+
+wcpa_compare_plot <- function(
+						.data,
+						control = "",
+						observe = "",
+						res = "2500000",
+						norm = "KR",
+						chr = c(1:22,"X","Y"),
+						axis_size = "10",
+						border_color = "#FFFFFF",
+						scales = "fixed",
+						min = NA,
+						min_color = "#3c5488",
+						max = NA,
+						max_color = "#e64b35"
+){
+	dt <- wcpa(.data)[
+				sample %in% c(observe,control),
+				.(
+					sample,
+					chr1 = factor(chr1,levels = chr),
+					chr2 = factor(chr2,levels = rev(chr)),
+					WCPA
+				)
+			] %>% 
+			mutate_when(sample == control,sample = "control") %>%
+			mutate_when(sample == observe,sample = "observe") %>%
+			wider_dt(name = "sample",value = "WCPA") %>%
+			mutate_dt(overlap = observe/control) 
+
+	if(is.na(min)){min <- min(dt$overlap)}
+	if(is.na(max)){max <- max(dt$overlap)}
+
+	dt %>%
+	ggplot(aes(chr1,chr2,fill = overlap)) + 
+	geom_tile(
+		color = border_color,
+		size = 0.2
+	) + 
+	scale_fill_gradient2(
+		limits = c(min,max),
+		low = "#3c5488",
+		mid = "#FFFFFF",
+		high = "#e64b35",
+		midpoint = 1,
+		breaks = seq(min,max,0.01)
+	) + 
+	labs(
+		title = paste(observe,control,sep = " / "),
+		x = element_blank(),
+		y = element_blank()
+	) + 
+	theme(
+		plot.title = element_text(hjust = 0.5),
+		plot.background = element_blank(),
+		panel.grid.major = element_blank(),
+		panel.background = element_blank(),
+		panel.grid.minor = element_blank(),
+		axis.ticks.x = element_blank(), 
+		axis.ticks.y = element_blank(),
+		axis.text.x = element_text(size = axis_size),
+		axis.text.y = element_text(size = axis_size),
+		legend.title = element_blank(),
+		legend.key.widt = unit(0.4,"cm")
+	) 
+}
+
+
