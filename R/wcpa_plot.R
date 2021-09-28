d994658ecef8755b5432a08cc2d47920679a7f1b
chaosfang404 <fc-ibb105@outlook.com>
2021-09-28 16:57:26 +0800

    rewrite wcpa_plot

--- R/wcpa_plot.R
+++ R/wcpa_plot.R
@@ -1,33 +1,76 @@
 wcpa_plot = function(
 				.data,
-				name,
+				name = NA,
 				res = "2500000",
 				norm = "KR",
-				chr = c(1:22,"X","Y")
-){
-	wcpa_matrix(
-				.data,
-				name,
-				res,
-				norm,
-				chr
-			) %>%
-		pheatmap( 
-				cluster_rows = F, 
-				cluster_cols = F,
-				legend = T, 
-				main = name,
-				display_numbers = F, 
-				fontsize = 6, 
-				fontsize_row = 10, 
-				fontsize_col = 10, 
-				number_color = "black",
-				number_format = "%.1f",
-				angle_col ="0",
-				color = colorRampPalette(colors = c("#3c5488","white","#e64b35"))(1000),
-				scale = "none",
+				chr = c(1:22,"X","Y"),
+				font_size = "10",
 				border_color = "white",
-				border = T
-				#在指定了border_color之后，border=F会被忽略掉，当不需要border时，必须注释掉border_color
-			)
+				scales = "fixed"
+){
+	dt <- wcpa(.data)[
+				,chr1 := factor(chr1,levels = chr)
+			][
+				,chr2 := factor(chr2,levels = rev(chr))
+			][
+				resolution == res &
+				normalization == norm
+			]
+
+	if(length(name) > 1)
+	{
+		dt <- dt[
+				sample %in% name
+			][
+				,sample := factor(sample,levels = name)
+			]
+	} else if(length(name) == 1)
+	{
+		if(!is.na(name))
+		{
+			dt <- dt[sample == name]
+		}
+	}
+
+	p_base <- dt %>%
+				ggplot(aes(chr1,chr2,fill = WCPA)) + 
+				geom_tile(
+					color = border_color,
+					size = 0.2
+				) + 
+				scale_fill_gradient2(
+					low = "#3c5488",
+					mid = "white",
+					high = "#e64b35",
+					midpoint = mean(c(min(dt$WCPA),max(dt$WCPA)))
+				) + 
+				labs(
+					x = element_blank(),
+					y = element_blank()
+				) + 
+				theme(
+					plot.background = element_blank(),
+					panel.grid.major = element_blank(),
+					panel.background = element_blank(),
+					panel.grid.minor = element_blank(),
+					axis.ticks.x = element_blank(), 
+					axis.ticks.y = element_blank(),
+					axis.text.x = element_text(size = font_size),
+					axis.text.y = element_text(size = font_size)
+				)
+
+	if(length(unique(dt$sample)) == 1)
+	{
+		p_base
+
+	}else
+	{
+		p_base + 
+		facet_wrap(
+			~sample,
+			ncol = 2,
+			scales = scales
+		)
+	}
+
 }
