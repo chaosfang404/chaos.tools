5786c7c80717256371520faa3b51c68e13019cfa
chaosfang404 <fc-ibb105@outlook.com>
2021-10-12 11:24:11 +0800

    bug fix in wcpa.R

--- R/wcpa.R
+++ R/wcpa.R
@@ -58,36 +58,36 @@
 # }
 
 
-wcpa <- function(.data)
-{
-	dt <- data.table(.data) %>%
-			setnames(
-				c("sample","resolution","normalization","chr1","chr2","interaction")
-		)
-	dt[
-		chr1 != chr2
-	][
-		,
-		.(chr1,chr2,interaction,sample_total = sum(interaction)/2),
-		.(sample,resolution,normalization)
-	][
-		,
-		.(chr2,interaction,sample_total,chr1_total = sum(interaction)),
-		.(sample,resolution,normalization,chr1)
-	][
-		,
-		.(chr1,interaction,sample_total,chr1_total,chr2_total = sum(interaction)),
-		.(sample,resolution,normalization,chr2)
-	][
-		,
-		.(sample,resolution,normalization,chr1,chr2,interaction,sample_total,chr1_total,chr2_total)
-	][
-		order(sample,resolution,normalization,chr1,chr2)
-	][
-		,
-		WCPA := interaction/(((chr1_total/sample_total)*(chr2_total/(sample_total - chr1_total)) + (chr2_total/sample_total)*(chr1_total/(sample_total - chr2_total))) * sample_total/2)
-	]
-}
+# wcpa <- function(.data)
+# {
+# 	dt <- data.table(.data) %>%
+# 			setnames(
+# 				c("sample","resolution","normalization","chr1","chr2","interaction")
+# 		)
+# 	dt[
+# 		chr1 != chr2
+# 	][
+# 		,
+# 		.(chr1,chr2,interaction,sample_total = sum(interaction)/2),
+# 		.(sample,resolution,normalization)
+# 	][
+# 		,
+# 		.(chr2,interaction,sample_total,chr1_total = sum(interaction)),
+# 		.(sample,resolution,normalization,chr1)
+# 	][
+# 		,
+# 		.(chr1,interaction,sample_total,chr1_total,chr2_total = sum(interaction)),
+# 		.(sample,resolution,normalization,chr2)
+# 	][
+# 		,
+# 		.(sample,resolution,normalization,chr1,chr2,interaction,sample_total,chr1_total,chr2_total)
+# 	][
+# 		order(sample,resolution,normalization,chr1,chr2)
+# 	][
+# 		,
+# 		WCPA := interaction/(((chr1_total/sample_total)*(chr2_total/(sample_total - chr1_total)) + (chr2_total/sample_total)*(chr1_total/(sample_total - chr2_total))) * sample_total/2)
+# 	]
+# }
 
 
 wcpa_plot <- function(
