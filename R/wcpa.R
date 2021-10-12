07a68f16b96dd0ca4925b71298b6002dcbd43b31
chaosfang404 <fc-ibb105@outlook.com>
2021-10-12 11:22:17 +0800

    bug fix in wcpa.R

--- R/wcpa.R
+++ R/wcpa.R
@@ -192,105 +192,105 @@ wcpa_plot <- function(
 
 
 
-wcpa_compare_plot <- function(
-						.data,
-						control,
-						observe,
-						res = 2500000,
-						norm = "KR",
-						chr = c(1:22,"X","Y"),
-						axis_size = 10,
-						border_color = "#FFFFFF",
-						scales = "fixed",
-						min = NA,
-						min_color = "#3c5488",
-						max = NA,
-						max_color = "#e64b35"
-){
-	dt <- wcpa(.data) %>%
-			.[
-				sample %in% c(observe,control) &
-				normalization == norm &
-				resolution == res,
-				.(sample, chr1, chr2, WCPA)
-			] %>%
-			complete_dt(
-				sample = c(observe,control),
-				chr1 = chr,
-				chr2 = chr,
-				fill = NA
-			) %>% 
-			.[, chr1 := factor(chr1,levels = chr)] %>%
-			.[, chr2 := factor(chr2,levels = rev(chr))] %>%
-			.[sample == control, sample := "control"] %>%
-			.[sample == observe, sample := "observe"] %>%
-			wider_dt(name = "sample",value = "WCPA") %>%
-			.[,overlap := observe/control] 
+# wcpa_compare_plot <- function(
+# 						.data,
+# 						control,
+# 						observe,
+# 						res = 2500000,
+# 						norm = "KR",
+# 						chr = c(1:22,"X","Y"),
+# 						axis_size = 10,
+# 						border_color = "#FFFFFF",
+# 						scales = "fixed",
+# 						min = NA,
+# 						min_color = "#3c5488",
+# 						max = NA,
+# 						max_color = "#e64b35"
+# ){
+# 	dt <- wcpa(.data) %>%
+# 			.[
+# 				sample %in% c(observe,control) &
+# 				normalization == norm &
+# 				resolution == res,
+# 				.(sample, chr1, chr2, WCPA)
+# 			] %>%
+# 			complete_dt(
+# 				sample = c(observe,control),
+# 				chr1 = chr,
+# 				chr2 = chr,
+# 				fill = NA
+# 			) %>% 
+# 			.[, chr1 := factor(chr1,levels = chr)] %>%
+# 			.[, chr2 := factor(chr2,levels = rev(chr))] %>%
+# 			.[sample == control, sample := "control"] %>%
+# 			.[sample == observe, sample := "observe"] %>%
+# 			wider_dt(name = "sample",value = "WCPA") %>%
+# 			.[,overlap := observe/control] 
+# 
+# 	if(is.na(min)){min <- min(dt$overlap)}
+# 	if(is.na(max)){max <- max(dt$overlap)}
+# 
+# 	dt %>%
+# 	ggplot(aes(chr1,chr2,fill = overlap)) + 
+# 	geom_tile(
+# 		color = border_color,
+# 		size = 0.2
+# 	) + 
+# 	scale_fill_gradient2(
+# 		limits = c(min,max),
+# 		low = "#3c5488",
+# 		mid = "#FFFFFF",
+# 		high = "#e64b35",
+# 		midpoint = 1,
+# 		#breaks = seq(min,max,0.1)
+# 	) + 
+# 	labs(
+# 		title = paste(observe,control,sep = " / "),
+# 		x = element_blank(),
+# 		y = element_blank()
+# 	) + 
+# 	theme(
+# 		plot.title = element_text(hjust = 0.5),
+# 		plot.background = element_blank(),
+# 		panel.grid.major = element_blank(),
+# 		panel.background = element_blank(),
+# 		panel.grid.minor = element_blank(),
+# 		axis.ticks.x = element_blank(), 
+# 		axis.ticks.y = element_blank(),
+# 		axis.text.x = element_text(size = axis_size),
+# 		axis.text.y = element_text(size = axis_size),
+# 		legend.title = element_blank(),
+# 		legend.key.widt = unit(0.4,"cm")
+# 	) 
+# }
 
-	if(is.na(min)){min <- min(dt$overlap)}
-	if(is.na(max)){max <- max(dt$overlap)}
 
-	dt %>%
-	ggplot(aes(chr1,chr2,fill = overlap)) + 
-	geom_tile(
-		color = border_color,
-		size = 0.2
-	) + 
-	scale_fill_gradient2(
-		limits = c(min,max),
-		low = "#3c5488",
-		mid = "#FFFFFF",
-		high = "#e64b35",
-		midpoint = 1,
-		#breaks = seq(min,max,0.1)
-	) + 
-	labs(
-		title = paste(observe,control,sep = " / "),
-		x = element_blank(),
-		y = element_blank()
-	) + 
-	theme(
-		plot.title = element_text(hjust = 0.5),
-		plot.background = element_blank(),
-		panel.grid.major = element_blank(),
-		panel.background = element_blank(),
-		panel.grid.minor = element_blank(),
-		axis.ticks.x = element_blank(), 
-		axis.ticks.y = element_blank(),
-		axis.text.x = element_text(size = axis_size),
-		axis.text.y = element_text(size = axis_size),
-		legend.title = element_blank(),
-		legend.key.widt = unit(0.4,"cm")
-	) 
-}
-
-
-## wcpa_matrix <- function(
-## 				.data,
-## 				name,
-## 				res = 2500000,
-## 				norm = "KR",
-## 				chr = c(1:22,"X","Y")
-## ){
-## 	dt <- wcpa(.data)[
-## 				sample == name & 
-## 				resolution == res &
-## 				normalization == norm &
-## 				chr1 %in% chr & 
-## 				chr2 %in% chr
-## 		][
-## 			, 
-## 			.(
-## 				chr1 = factor(chr1,levels = chr),
-## 				chr2 = factor(chr2,levels = chr),
-## 				WCPA
-## 			)
-## 		] %>%
-## 		melt(
-## 			"chr1",
-## 			variable.name = "chr2",
-## 			value.name = "WCPA"
-## 		) %>%
-## 		setnames("chr","chr1")
-## 	dt
-## }
+#wcpa_matrix <- function(
+#				.data,
+#				name,
+#				res = 2500000,
+#				norm = "KR",
+#				chr = c(1:22,"X","Y")
+#){
+#	dt <- wcpa(.data)[
+#				sample == name & 
+#				resolution == res &
+#				normalization == norm &
+#				chr1 %in% chr & 
+#				chr2 %in% chr
+#		][
+#			, 
+#			.(
+#				chr1 = factor(chr1,levels = chr),
+#				chr2 = factor(chr2,levels = chr),
+#				WCPA
+#			)
+#		] %>%
+#		melt(
+#			"chr1",
+#			variable.name = "chr2",
+#			value.name = "WCPA"
+#		) %>%
+#		setnames("chr","chr1")
+#	dt
+#}
