473805de15d40722b88066d1be1a86b108c41694
chaosfang404 <fc-ibb105@outlook.com>
2021-10-20 21:26:28 +0800

    combine hic_inter & hic_tra function

--- R/hic_interactions.R
+++ R/hic_interactions.R
@@ -1,50 +1,51 @@
-hic_inter <- function(
-				hic_file,
-				chr_list = c(1:22,"X","Y"),
-				norm = "NONE",
-				resolution = 1e6
+hic_interaction <- function(
+						hic_file,
+						chr_list = c(1:22,"X","Y"),
+						norm = "KR",
+						resolution = 1e6,
+						inter = "all"
 ){
-	chr_list <- as.character(chr_list)
+	chr_list <- chr_list %>% unique() %>% as.character()
 
-	if(length(chr_list == 1))
+	if(length(chr_list) == 1)
 	{
-		chr_list <- rep(chr_list,2)
+		chr_list_dt <- chr_list %>% rep(2) %>% combn(2) %>% t() %>% data.table()
+
+	}else
+	{
+		if(inter == "inter")
+		{
+			chr_list_dt <- chr_list %>% combn(2) %>% t() %>% data.table()
+
+		}else if(inter == "intra")
+		{
+			chr_list_dt <- data.table(V1 = chr_list, V2 = chr_list)
+
+		}else if(inter == "all")
+		{
+			chr_list_dt <- rbind( 
+								chr_list %>% combn(2) %>% t() %>% data.table(),
+								data.table(V1 = chr_list,V2 = chr_list)
+							)
+		}
 	}
 
-	chr_list_dt <- combn(chr_list,2) %>% 
-					t() %>% 
-					data.table()
-
-	inter_data <- data.table(NULL)
+	data <- data.table(NULL)
 	for (i in 1:nrow(chr_list_dt))
 	{
 		chr1 <- chr_list_dt[i,V1]
 		chr2 <- chr_list_dt[i,V2]
-		tmp <- strawr::straw(norm, hic_file, chr1, chr2, "BP", resolution) %>%
-				data.table() %>%
-				mutate_dt(chr1 = chr1,chr2 = chr2) %>%
-				replace_na_dt(to = 0)
-		inter_data <- rbind(inter_data,tmp)
-	}
-	inter_data
-}
 
-hic_intra <- function(
-				hic_file,
-				chr_list = c(1:22,"X","Y"),
-				norm = "NONE",
-				resolution = 1e6
-){
-	chr_list <- as.character(chr_list)
-
-	intra_data <- data.table(NULL)
-	for (i in 1:length(chr_list))
-	{
-		tmp <- strawr::straw(norm, hic_file, i, i, "BP", resolution) %>%
-				data.table() %>%
-				mutate_dt(chr1 = i,chr2 = i) %>%
+		tmp <- data.table(
+					strawr::straw(norm, hic_file, chr1, chr2, "BP", resolution)
+				)[
+					,chr1 := chr1
+				][
+					,chr2 := chr2
+				] %>%
+				rename_dt(chr1_bin =x, chr2_bin = y) %>%
 				replace_na_dt(to = 0)
-		intra_data <- rbind(intra_data,tmp)
+		data <- rbind(data,tmp)
 	}
-	intra_data
+	data
 }
