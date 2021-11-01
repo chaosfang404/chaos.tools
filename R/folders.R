67728857e598b09fe832067fb3e269d55c441e7d
chaosfang404 <fc-ibb105@outlook.com>
2021-11-01 20:44:46 +0800

    update desktop.R & op.R

--- R/folders.R
+++ R/folders.R
@@ -11,27 +11,26 @@ scratch <- function()
 }
 
 desktop <- function(
-			user_name = NA
+			username = NA
 ){
 	if(is.na(username))
 	{
-		user_name <- Sys.info()[7]
+		username <- Sys.info()[7]
 	}
 
 	platform <- Sys.info()[1]
-	release_name <- Sys.info()[2]
 
 	if(platform == "Windows")
 	{
-		desk_dir <- paste0("C:/Users/",user_name,"/Desktop")
+		desk_dir <- paste0("C:/Users/",username,"/Desktop")
 	}else if(platform == "Linux")
 	{
 		if(!stringr::str_detect(Sys.info()[2],"WSL"))
 		{
-			desk_dir <- paste0("~/",user_name, "/Desktop")
+			desk_dir <- paste0("~/",username, "/Desktop")
 		}else
 		{
-			desk_dir <- paste0("/mnt/c/",user_name,"/Desktop")
+			desk_dir <- paste0("/mnt/c/",username,"/Desktop")
 		}
 	}
 
