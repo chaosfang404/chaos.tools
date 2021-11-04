d1db3d9f94e68b599183151fcb444789adec9596
chaosfang404 <fc-ibb105@outlook.com>
2021-11-04 13:06:32 +0800

    rewrite comb.R

--- R/comb.R
+++ R/comb.R
@@ -1,47 +1,19 @@
-comb2 <- function(
-			...,
-			sep = "_"
+dt_comb <- function(
+				...
 ){
-	list_all <- list(...)
-
-	all <- list_all[[1]]
-
-	if (length(list_all) >= 2)
-	{
-		for (i in 2:length(list_all))
-		{
-			paste2 <- function(
-						x
-			){
-				paste(x,list_all[[i]],sep = sep)
-			}
-		
-			all <- sapply(all,paste2)
-		}
-	}
-	all <- t(all)
-	rownames(all) <- NULL
-	data.table(all)
+	sapply(
+		list(...),
+		function(x){rep(x,length.out = max(sapply(list(...),length)))}
+	) %>% 
+	data.table() %>% 
+	complete_dt()
 }
 
 str_comb <- function(
 				...,
 				sep = "_"
 ){
-	str <- comb2(...,sep = sep) %>% unlist
-	names(str) <- NULL
-	str
+	dt_comb(...) %>% 
+	unite_dt(colnames(.)) %>% 
+	.[,V1]
 }
-
-
-dt_comb <- function(
-				...,
-				sep = "_"
-){
-	str_comb(...,sep = sep) %>%
-	data.table() %>%
-	separate_col(".") %>% 
-	setnames(paste0("V",1:ncol(.))) %>% 
-	.[]
-}
-
