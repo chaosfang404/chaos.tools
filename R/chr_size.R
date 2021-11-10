a548f9071643f4cb28898ccf51bc49f800dd06a8
chaosfang404 <fc-ibb105@outlook.com>
2021-11-10 10:07:30 +0800

    rewrite the online module of chr_size.R

--- R/chr_size.R
+++ R/chr_size.R
@@ -1,19 +1,31 @@
+online_genome_process <- function(
+							ref
+){
+	paste0("https://hgdownload.soe.ucsc.edu/goldenPath/",ref,"/bigZips/",ref,".chrom.sizes") %>%
+	fread() %>%
+	setnames(c("chr","length")) %>%
+	.[,name := ref] %>%
+	.[str_detect(chr,"_"),group := "extra"] %>%
+	.[chr == "chrM", group := "mitochondrion"] %>%
+	.[is.na(group), group := "main",.(name,chr,length,group)] %>%
+	.[,group := factor(group,levels = c("main","mitochondrion","extra"))] %>%
+	.[order(group,chr)]
+}
+
 chr_size <- function(
 				ref = "hg19",
 				mit = FALSE,
 				extra = FALSE,
 				online = FALSE
 ){
-	all_genome <- system.file(
+	local_genome <- system.file(
 						"extdata",
 						"chr_size.info.txt",
 						package = "chaos.tools"
 					) %>%
 					fread()
 
-	genome_list <- all_genome[,V1] %>% unique()
-
-	ucsc_url <- paste0("https://hgdownload.soe.ucsc.edu/goldenPath/",ref,"/bigZips/",ref,".chrom.sizes")
+	genome_list <- local_genome[,name] %>% unique()
 
 	if (ref == "list")
 	{
@@ -25,29 +37,16 @@ chr_size <- function(
 		{
 			if(isTRUE(online))
 			{
-				dt <- fread(ucsc_url) %>%
-						setnames(c("chr","length")) %>%
-						.[,name := ref] %>%
-						.[str_detect(chr,"_"),group := "extra"] %>%
-						.[chr == "chrM", group := "mitochondrion"] %>%
-						.[is.na(group), group := "main",.(name,chr,length,group)] %>%
-						.[order(chr)]
+				dt <- online_genome_process(ref = ref)
 			}else
 			{
-				dt <- all_genome[
-							V1 == ref,
-							.(name = V1,chr = V2,length = V3,group = V4)
+				dt <- local_genome[
+							name == ref,group := factor(group,levels = c("main","mitochondrion","extra"))
 						]
 			}
 		}else
 		{
-			dt <- fread(ucsc_url) %>%
-					setnames(c("chr","length")) %>%
-					.[,name := ref] %>%
-					.[str_detect(chr,"_"),group := "extra"] %>%
-					.[chr == "chrM", group := "mitochondrion"] %>%
-					.[is.na(group), group := "main",.(name,chr,length,group)] %>%
-					.[order(chr)]
+			dt <- online_genome_process(ref = ref)
 		}
 
 		if(isFALSE(mit))
