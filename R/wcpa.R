ac677988f45785e97c8449d632c61410b3a1ea09
fc-ibb105 <fc-ibb105@outlook.com>
2021-12-29 16:12:38 +0800

    update wcpa plot

--- R/wcpa.R
+++ R/wcpa.R
@@ -218,10 +218,8 @@ wcpa_plot <- function(
 					panel.grid.major = element_blank(),
 					panel.background = element_blank(),
 					panel.grid.minor = element_blank(),
-					axis.ticks.x = element_blank(), 
-					axis.ticks.y = element_blank(),
-					axis.text.x = element_text(size = axis_size),
-					axis.text.y = element_text(size = axis_size),
+					axis.ticks = element_blank(), 
+					axis.text = element_text(size = axis_size),
 					legend.key.width = unit(0.4,"cm"),
 					legend.title = element_blank()
 				)
@@ -312,10 +310,8 @@ wcpa_compare_plot <- function(
 		panel.grid.major = element_blank(),
 		panel.background = element_blank(),
 		panel.grid.minor = element_blank(),
-		axis.ticks.x = element_blank(), 
-		axis.ticks.y = element_blank(),
-		axis.text.x = element_text(size = axis_size),
-		axis.text.y = element_text(size = axis_size),
+		axis.ticks = element_blank(), 
+		axis.text = element_text(size = axis_size),
 		legend.title = element_blank(),
 		legend.key.widt = unit(0.4,"cm")
 	) 
