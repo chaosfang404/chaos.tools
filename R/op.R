330bc4113a79ab68b9e75a1b632816c9080dcf00
fc-ibb105 <fc-ibb105@outlook.com>
2022-01-06 08:59:19 +0800

    update

--- R/op.R
+++ R/op.R
@@ -10,10 +10,10 @@ op <- function(
 	if(ext == "pdf")
 	{
 		program <- "/mnt/c/Users/Chao/AppData/Local/SumatraPDF/SumatraPDF.exe"
-	}else if (ext == "png" | ext == "jpeg" | ext == "bmp")
+	} else if (ext == "png" | ext == "jpeg" | ext == "bmp")
 	{
 		program <- "/mnt/c/Program Files/Honeyview/Honeyview.exe"
-	}else
+	} else
 	{
 		program <- "/mnt/c/Program Files/Sublime Text/subl.exe"
 	}
@@ -21,3 +21,11 @@ op <- function(
 	cmd <- paste(shQuote(program),file,sep = " ")
 	system(cmd,inter = F)
 }
+
+##  else if (ext == "ppt" | ext == "pptx")
+## {
+## 	program <- "/mnt/c/Program\ Files/Microsoft\ Office/root/Office16/POWERPNT.EXE"
+## } else if(ext == "xls" | ext == "xlsx" | ext == "csv")
+## {
+## 	program <- "/mnt/c/Program\ Files/Microsoft\ Office/root/Office16/EXCEL.EXE"
+## }
