408eb729d7b79804747ececdc348790ea65049d0
fc-ibb105 <fc-ibb105@outlook.com>
2022-02-23 20:48:23 +0800

    fix a bug when chr_list mixed real chr and NA/NaN

--- R/chr_list_dt.R
+++ R/chr_list_dt.R
@@ -5,22 +5,30 @@ chr_list_dt <- function(
 					mit = FALSE,
 					inter = "half"
 ){
+	hic_file_chr <-	hic_file %>%
+					strawr::readHicChroms() %>%
+					.$name %>%
+					.[!. %in% c("ALL","All")]
+
 	chr_list <- chr_list %>% 
 				unique() %>% 
 				as.character() %>%
-				.[! . %in% c(NA,NaN)]
+				.[! . %in% c(NA,NaN)] %>%
+				gsub("chr","",.)
 
-	if(length(chr_list) == 1 | length(chr_list) == 0)
+	if(length(chr_list) == 0)
+	{
+		chr_list <- NA
+	}
+
+	if(length(chr_list) == 1)
 	{
 		if(!is.na(chr_list))
 		{
 			inter <- "intra"
 		}else
 		{
-			chr_list <- hic_file %>%
-						strawr::readHicChroms() %>%
-						.$name %>%
-						.[!. %in% c("ALL","All")]
+			chr_list <- hic_file_chr
 
 			if(isFALSE(extra))
 			{
@@ -31,6 +39,16 @@ chr_list_dt <- function(
 			{
 				chr_list <- chr_list[!str_detect(chr_list,"M")]
 			}
+		}
+	}else
+	{
+		hic_chr_symbol <-	hic_file_chr %>%
+							sample(1) %>%
+							str_detect("chr")
+
+		if(isTRUE(hic_chr_symbol))
+		{
+			chr_list <- paste0("chr",chr_list)
 		}
 	}
 
