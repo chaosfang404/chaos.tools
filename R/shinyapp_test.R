0aa75b3f15cc7af796499c3a12f48a0dbb223f70
chaosfang404 <83016338+chaosfang404@users.noreply.github.com>
2022-05-31 16:15:55 +0800

    add rna_ui()

--- R/shinyapp_test.R
+++ R/shinyapp_test.R
@@ -81,3 +81,71 @@ cvd_ui <- function(cvd_data)
 				)
 	shinyApp(cvd_ui,cvd_server)
 }
+
+rna_ui <- function(.data)
+{
+	rna_server <-	function(
+						input,
+						output
+	){
+		output$rna_caption <-	renderText(
+								{
+									"Volcano plot"
+								})
+	
+		output$rna_plot <-	renderPlot(
+							{
+								volcano_plot(
+									.data,
+									top_gene_number = input$top_gene_number,
+									fold_change = input$fold_change,
+									p_value = input$p_value
+								)
+							})
+		output$rna_table <- renderDataTable({
+									.data
+							})
+	}
+
+	rna_ui <-	fluidPage(
+					titlePanel("Transcriptome"),
+					sidebarLayout(
+						sidebarPanel(					
+							sliderInput(
+								"top_gene_number",
+								"Top gene number",
+								value = 10,
+								min = 0,
+								max = 100,
+								step = 1,
+								animate = T
+							),
+							sliderInput(
+								"fold_change",
+								"fold change",
+								value = 1.5,
+								min = 0,
+								max = 5,
+								step = 0.1,
+								animate = T
+							),
+							sliderInput(
+								"p_value",
+								"P value",
+								value = 0.05,
+								min = 0.001,
+								max = 0.5,
+								step = 0.001,
+								animate = T
+							)
+						),
+						mainPanel(
+							h3(textOutput("rna_caption")),
+							plotOutput("rna_plot"),
+							h3("Data"),
+							dataTableOutput("rna_table")
+						)
+					)
+				)
+	shinyApp(rna_ui,rna_server)
+}
